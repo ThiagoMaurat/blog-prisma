ab5f78ace03b8b12f826fab381fcb4cb19a275bd
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-06-25 18:17:37 -0300

    feat:shadcn / darkcolor

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,12 +1,23 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
 interface Props {
   children: React.ReactNode;
 }
 
-const Provider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const Provider = ({ children, ...props }: ThemeProviderProps) => {
+  return (
+    <NextThemesProvider
+      {...props}
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+    >
+      <SessionProvider>{children}</SessionProvider>;
+    </NextThemesProvider>
+  );
 };
 
 export default Provider;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-
-const {fontFamily} = require("tailwindcss/defaultTheme");
-
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
-    extend: {
+    container: {
+      center: true,
+      padding: "2rem",
       screens: {
-        sm: '30em',  // 480px
-        md: '48em',  // 768px
-        lg: '62em',  // 992px
-        xl: '80em',  // 1280px
+        "2xl": "1400px",
       },
-      fontFamily: {
-        primary: ["var(--font-montserrat)", fontFamily.sans],
-      },
-      html: {
-        margin: "0",
-        padding: "0",
-        boxSizing: "border-box",
-        overflow: "auto",
-        "&::-webkit-scrollbar": {
-          width: "6px",
-          height: "8px",
-        },
-        "&::-webkit-scrollbar-track": {
-          background: "#D9D9D9",
-          borderRadius: "17px",
-          height: "8px",
-        },
-        "&::-webkit-scrollbar-thumb": {
-          background: "#60606C",
-          borderRadius: "17px",
-          height: "8px",
-        },
-        "&::-webkit-scrollbar-corner": {
-          background: "rgba(0,0,0,0)",
-        },
-      },
+    },
+    extend: {
       colors: {
         gray: {
           300: "#40ADF4",
@@ -61,7 +34,60 @@ module.exports = {
         customgray: {
           500: "#BBBBBB",
         },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-};
+  plugins: [require("tailwindcss-animate")],
+}
