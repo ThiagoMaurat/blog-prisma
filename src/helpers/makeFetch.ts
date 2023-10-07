377f47b196d3b61ec978daf469a6ff6704151e24
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-10-07 00:38:40 -0300

    fix:make fetch error

--- src/helpers/makeFetch.ts
+++ src/helpers/makeFetch.ts
@@ -2,7 +2,7 @@ export async function makeFetch<T = unknown>(
   url: RequestInfo | URL,
   init?: RequestInit | undefined
 ): Promise<T> {
-  const data = await fetch(`${process.env.NEXTAUTH_URL}${url}`, init);
+  const data = await fetch(`${url}`, init);
 
   const result = await data.json();
 
