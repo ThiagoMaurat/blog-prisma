919d40799a581998d2a2b7da4836c42b4f8726a9
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-10-08 19:07:28 -0300

    fix:url env fetch

--- src/helpers/makeFetch.ts
+++ src/helpers/makeFetch.ts
@@ -2,7 +2,7 @@ export async function makeFetch<T = unknown>(
   url: RequestInfo | URL,
   init?: RequestInit | undefined
 ): Promise<T> {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, init);
+  const data = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}${url}`, init);
 
   const result = await data.json();
 
