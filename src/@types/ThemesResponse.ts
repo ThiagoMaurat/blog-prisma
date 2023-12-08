319b06c7f49aeaa784c3c610e57bdbe6d5de74c4
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-12-08 19:08:12 -0300

    chore:removed unusual code

--- src/@types/ThemesResponse.ts
+++
@@ -1,8 +0,0 @@
-export interface Theme {
-  id: string;
-  name: string;
-}
-
-export interface ThemeResponse {
-  themes: Theme[];
-}
--- src/server/factories/make-validate-email-code.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaUsersRepository } from "../repositories/prisma/users-repository";
-import { ValidateCodeEmail } from "../use-cases/Authenticate/ValidateCodeEmail";
-
-export function makeValidateEmailCode() {
-  const prismaPostRepository = new PrismaUsersRepository();
-  const validateCodeEmail = new ValidateCodeEmail(prismaPostRepository);
-
-  return validateCodeEmail;
-}
